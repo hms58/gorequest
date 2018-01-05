d183c525ee984f75092eb9132fb9c1e8bc12ef63
simon <hemengsi123@126.com>
2018-01-05 16:38:44 +0800

    添加不对URL query encode 操作

--- gorequest.go
+++ gorequest.go
@@ -90,6 +90,7 @@ type SuperAgent struct {
 	}
 	//If true prevents clearing Superagent data and makes it possible to reuse it for the next requests
 	DoNotClearSuperAgent bool
+	enableEncodeQuery    bool
 }
 
 var DisableTransportSwap = false
@@ -121,6 +122,7 @@ func New() *SuperAgent {
 		Debug:             debug,
 		CurlCommand:       false,
 		logger:            log.New(os.Stderr, "[gorequest]", log.LstdFlags),
+		enableEncodeQuery: true,
 	}
 	// disable keep alives by default, see this issue https://github.com/parnurzeal/gorequest/issues/75
 	s.Transport.DisableKeepAlives = true
@@ -169,6 +171,7 @@ func (s *SuperAgent) ClearSuperAgent() {
 	s.TargetType = TypeJSON
 	s.Cookies = make([]*http.Cookie, 0)
 	s.Errors = nil
+	s.enableEncodeQuery = true
 }
 
 // Just a wrapper to initialize SuperAgent instance by method string
@@ -1276,14 +1279,15 @@ func (s *SuperAgent) MakeRequest() (*http.Request, error) {
 	}
 
 	// Add all querystring from Query func
-	q := req.URL.Query()
-	for k, v := range s.QueryData {
-		for _, vv := range v {
-			q.Add(k, vv)
+	if s.enableEncodeQuery {
+		q := req.URL.Query()
+		for k, v := range s.QueryData {
+			for _, vv := range v {
+				q.Add(k, vv)
+			}
 		}
+		req.URL.RawQuery = q.Encode()
 	}
-	req.URL.RawQuery = q.Encode()
-
 	// Add basic auth
 	if s.BasicAuth != struct{ Username, Password string }{} {
 		req.SetBasicAuth(s.BasicAuth.Username, s.BasicAuth.Password)
@@ -1309,4 +1313,9 @@ func (s *SuperAgent) AsCurlCommand() (string, error) {
 		return "", err
 	}
 	return cmd.String(), nil
+}
+
+func (s *SuperAgent) DisableEncodeQuery() *SuperAgent {
+	s.enableEncodeQuery = false
+	return s
 }
